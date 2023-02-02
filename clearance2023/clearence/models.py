c928be5222b3de447ae7ffae228444166712f17c
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-02-02 15:31:54 +0900

    feat: 모델 구현

--- clearance2023/clearence/models.py
+++ clearance2023/clearence/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    img = models.URLField()
+    url = models.URLField()
+    brand = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
+    sale_price = models.IntegerField()
+    normal_price = models.IntegerField()
+    discount_rate = models.IntegerField()
+    discount_type = models.CharField(max_length=15)
+    created_at = models.DateTimeField(auto_now_add=True)
